6c976103ee7a09d8225bc2feafdb9ad951e2b1fe
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-12-04 13:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 function convertToRoman(num) {
- let str=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    let str = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    let val = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
 
-let num=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
-
-let x=2739;
-let ptr=0;
-let ans="";
-while(x>0)
-{
-    let number=num[ptr];
-    if(x>=number)
-    {
-        ans=ans+str[ptr];
-        x=x-number;
+    let ptr = 0;
+    let ans = "";
+    while(num > 0) {
+        if(num >= val[ptr]) {
+            ans = ans + str[ptr];
+            num = num - val[ptr];
+        } else {
+            ptr++;
+        }
     }
-    else
-    {
-        ptr++;
-    }
+    return ans;
 }
-console.log(ans);
-   
-
-
-
+console.log(convertToRoman(2739));
